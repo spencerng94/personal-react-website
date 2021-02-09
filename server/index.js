a640f69ff74f92deec4c3108fae5fb3d6bd89f11
Spencer Ng <spencerng@spencers-mbp.lan>
2021-02-09 14:55:32 -0800

    Started environment setup for webpack

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../public/dist'));
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+  });
